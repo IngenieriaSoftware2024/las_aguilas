c18c1ef0bc49f2df30f024e2aa5703afa9211182
Maldonado <joseromaldo.1997@gmail.com>
2024-09-23 11:19:17 -0600

    Unión de sql en una sola carpeta script.sql

--- includes/script.sql
+++ includes/script.sql
@@ -22,18 +22,16 @@ CREATE TABLE empleado(
     emp_dpi VARCHAR (15),
     emp_direccion VARCHAR (80),
     emp_tel VARCHAR (10),
-    emp_situacion SMALLINT DEFAULT 1,
     PRIMARY KEY (emp_id)
 );
 
 CREATE TABLE usuario(
     usu_id SERIAL NOT NULL,
-    usu_nombre INTEGER,
+    usu_nombre VARCHAR (50),
     usu_catalogo INTEGER,
     usu_password LVARCHAR,
     usu_situacion SMALLINT DEFAULT 1,
-    PRIMARY KEY (usu_id),
-    FOREIGN KEY (usu_nombre) REFERENCES empleado (emp_id)
+    PRIMARY KEY (usu_id)
 );
 
 CREATE TABLE permiso(
@@ -45,3 +43,60 @@ CREATE TABLE permiso(
     FOREIGN KEY (per_usuario) REFERENCES usuario (usu_id),
     FOREIGN KEY (per_rol) REFERENCES rol (rol_id)
 );
+
+CREATE TABLE turnos (
+    turno_id SERIAL,
+    turno_empleado INTEGER,
+    turno_puesto INTEGER,
+    turno_fecha_inicio DATETIME YEAR TO MINUTE,
+    turno_fecha_fin DATETIME YEAR TO MINUTE,
+    turno_situacion SMALLINT DEFAULT 1,
+    PRIMARY KEY (turno_id),
+    FOREIGN KEY (turno_empleado) REFERENCES empleado(emp_id),
+    FOREIGN KEY (turno_puesto) REFERENCES puestos(puesto_id)
+);
+
+CREATE TABLE puestos(
+    puesto_id SERIAL,
+    puesto_nombre VARCHAR(50),
+    puesto_descripcion VARCHAR(150),
+    puesto_salario MONEY (10,2),
+    puesto_direccion VARCHAR (100),
+    puesto_cliente INTEGER,
+    puesto_situacion SMALLINT DEFAULT 1, 
+    PRIMARY KEY (puesto_id),
+    FOREIGN KEY (puesto_cliente) REFERENCES clientes(cliente_id)
+);
+ 
+create table clientes (
+    cliente_id SERIAL PRIMARY KEY,
+    cliente_nombre VARCHAR(100),
+    cliente_nit INTEGER,
+    cliente_propietario VARCHAR(100),
+    cliente_telefono INTEGER,
+    cliente_email VARCHAR(50),
+    cliente_ubicacion VARCHAR(100),
+    cliente_contrato VARCHAR(255)
+);
+
+CREATE TABLE encabezado_factura (
+    factura_id SERIAL PRIMARY KEY,
+    factura_correlativo VARCHAR(150),
+    factura_cliente INT,
+    factura_NIT INTEGER,
+    factura_mes INT,
+    factura_anio INT, 
+    factura_situacion SMALLINT DEFAULT 1,
+    FOREIGN KEY (factura_cliente) REFERENCES clientes(cliente_id)
+);
+
+CREATE TABLE detalle_factura (
+    detalle_id SERIAL PRIMARY KEY,
+    detalle_encabezado INT,
+    detalle_cantidad_empleados INTEGER,
+    detalle_empleados VARCHAR(100),
+    detalle_total INTEGER,
+    detalle_situacion SMALLINT DEFAULT 1,
+    FOREIGN KEY (detalle_encabezado) REFERENCES encabezado_factura(factura_id)
+);
+
