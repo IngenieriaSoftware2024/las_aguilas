956e99b8f09be0cba815f800fb12ef0f94d07edc
lucasartal56 <lucdanart@gmail.com>
2024-09-23 07:57:18 -0600

    Creación de Reporte Controller y Vista de Reporte PDF Turnos

---
+++ views/pdf/style.php
@@ -0,0 +1,69 @@
+<style>
+     body {
+        font-family: Arial, sans-serif;
+        font-size: 12px;
+        color: #333;
+    }
+
+    .encabezado {
+        text-align: center;
+        padding: 20px;
+        border-bottom: 2px solid #000;
+        margin-bottom: 30px;
+    }
+
+    .logo {
+        height: 60px;
+        float: left;
+    }
+
+    .empresa-info {
+        text-align: center;
+    }
+
+    .empresa-info h1 {
+        font-size: 24px;
+        margin: 0;
+    }
+
+    .empresa-info p {
+        margin: 0;
+        font-size: 12px;
+    }
+    .tabla-detalles {
+    width: 100%;
+    border-collapse: collapse;
+}
+
+    .tabla-detalles th,
+    .tabla-detalles td {
+        padding: 10px;
+        border: 1px solid #ddd;
+    }
+
+    .tabla-detalles th {
+        background-color: #f5f5f5;
+        text-align: center; /* Encabezados centrados */
+    }
+
+    .tabla-detalles td {
+        text-align: left; /* Contenido alineado a la izquierda */
+        word-wrap: break-word;
+    }
+
+    .pie {
+        margin-top: 40px;
+        text-align: center;
+        font-size: 12px;
+        color: #777;
+    }
+
+    .footer_h1{
+        text-align: center;
+        color: red;
+    }
+    .header_h1{
+        text-align: center;
+        color: red;
+    }
+</style>
