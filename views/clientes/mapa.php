0428df2fb3af408eba9f9d289cebd7e55b365e9b
Vasquez11494 <cvasquez857@gmail.com>
2024-09-23 22:27:07 -0600

    mapa finalizado

---
+++ views/clientes/mapa.php
@@ -0,0 +1,18 @@
+<h1 class="text-center" style="font-family: 'Cinzel', serif; text-shadow: 1px 1px 5px rgba(0, 0, 0, 0.5); font-size: 2.5rem; color: #0056b3;"><b>UBICACION DE CLIENTES </b></h1>
+<div class="row justify-content-center">
+    <div class="col-lg-9 p-3 border rounded" id="map" style="height: 600px; min-height: auto;">
+    </div>
+
+</div>
+
+<style>
+    .tooltip {
+    background-color: white;
+    border: 1px solid black;
+    border-radius: 5px;
+    padding: 5px;
+    font-weight: bold;
+}
+</style>
+
+<script src="<?= asset('./build/js/clientes/mapa.js') ?>"></script>
